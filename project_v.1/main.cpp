9011ad786540e18a691d642128edc1d6fcf34827
ProkopchukMaksym <prokopchuk.maksym@chnu.edu.ua>
2025-05-22 08:52:20 +0300

    Add files via upload

--- project_v.1/main.cpp
+++ project_v.1/main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 void insertionSort(int arr[], int size) {
     for (int i = 1; i < size; i++) {
-        int key = arr[i];
+        int key = arr[i];      
         int j = i - 1;
 
         while (j >= 0 && arr[j] > key) {
@@ -13,36 +13,4 @@ void insertionSort(int arr[], int size) {
 
         arr[j + 1] = key;
     }
-}
-
-int main() {
-    int n;
-
-    cout << "Enter the number of elements: ";
-    cin >> n;
-
-    if (n <= 0) {
-        cout << "Invalid size! Please enter a positive number." << endl;
-        return 1;
-    }
-
-    int* masA = new int[n];
-
-    cout << "Input " << n << " numbers: ";
-    for (int i = 0; i < n; i++) {
-        cin >> masA[i];
-    }
-
-    // Виклик сортування вставками
-    insertionSort(masA, n);
-
-    cout << "Sorted array: ";
-    for (int i = 0; i < n; i++) {
-        cout << masA[i] << " ";
-    }
-    cout << endl;
-
-    delete[] masA;
-
-    return 0;
 }
